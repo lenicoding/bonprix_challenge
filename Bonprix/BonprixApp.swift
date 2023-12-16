f7fafc92a8873c9bf0d19b3d56aec364bf537da6
Magdalena Reißner <magdakamrowska@gmail.com>
2023-12-16 16:03:55 +0100

    Initial Commit

---
+++ Bonprix/BonprixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonprixApp.swift
+//  Bonprix
+//
+//  Created by Magdalena Reißner on 16.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BonprixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
